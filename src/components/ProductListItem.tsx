1567733798fe25d5240c4b766410e4938a4eae04
KTAP8 <touch.apinankul@gmail.com>
2024-12-23 22:10:11 +0700

    pass data and alias path

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,11 +1,22 @@
 import { StyleSheet, Image } from "react-native";
 import Colors from "@/src/constants/Colors";
 import { Text, View } from "@/src/components/Themed";
+import { Product } from "../types";
 
-const ProductListItem = ({ product }) => {
+type ProductListItemProps = {
+  product: Product;
+};
+
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image}></Image>
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      ></Image>
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>

88b7fa77d394cb49d35a4927858146f770711b69
KTAP8 <touch.apinankul@gmail.com>
2024-12-24 10:55:37 +0700

    cart function

---
+++ src/app/cart.tsx
@@ -0,0 +1,23 @@
+import { View, Text, Platform, FlatList } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { useContext } from "react";
+import { useCart } from "../providers/CartProvider";
+import CartListItem from "../components/CartListItem";
+
+const CartScreen = () => {
+  const { items } = useCart();
+
+  return (
+    <View>
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <CartListItem cartItem={item}></CartListItem>}
+        contentContainerStyle={{ padding: 10, gap: 10 }}
+      ></FlatList>
+      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
+    </View>
+  );
+};
+
+export default CartScreen;

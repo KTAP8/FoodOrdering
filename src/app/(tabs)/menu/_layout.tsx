7208b1d9ae1a45e133e2df7999446160f715af89
KTAP8 <touch.apinankul@gmail.com>
2024-12-24 09:48:49 +0700

    product details page

--- src/app/(tabs)/menu/_layout.tsx
+++ src/app/(tabs)/menu/_layout.tsx
@@ -1,5 +1,9 @@
 import { Stack } from "expo-router";
 
 export default function MenuStack() {
-  return <Stack></Stack>;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Menu" }}></Stack.Screen>
+    </Stack>
+  );
 }
